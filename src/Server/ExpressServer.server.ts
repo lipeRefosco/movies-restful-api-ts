58b10cad5b2b450ec05fa4abbef234505c972073
lipeRefosco <luisfelipe.refosco@hotmail.com>
2024-03-16 04:36:32 -0300

    refact[Server/ExpressServer]: add dependency inversion of ExpressServer Adapter

--- src/Server/ExpressServer.server.ts
+++ src/Server/ExpressServer.server.ts
@@ -1,15 +1,21 @@
 import { Express } from "express"
-import { env } from "../../env"
+
+import { IServerConfiguration } from "./IServerConfiguration.server"
 
 export class ExpressServer {
 
-    private _port: number = env.SERVER_PORT || 3000
+    private _port: number;
 
-    constructor(private _server: Express) { }
+    constructor(
+        private _server: Express,
+        { port }: IServerConfiguration
+    ) {
+        this._port = port;
+    }
 
     public listen(): void {
         this._server.listen(this._port, () => {
-            console.log(`Server running at: http://localhost:${this._port}`)
+            console.log(`Server running at: http://localhost:${this._port}`);
         })
     }
 }
