bd5b7134c5060492c5a68845a5a678f5a14d7dc1
lipeRefosco <luisfelipe.refosco@hotmail.com>
2024-03-16 04:38:57 -0300

    refact: adding server configs on constructor of ExpresServer Adapter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import express, { } from "express"
 import { ExpressServer } from "./Server/ExpressServer.server"
+import { IServerConfiguration } from "./Server/IServerConfiguration.server"
+import { env } from "../env"
 
-const app: ExpressServer = new ExpressServer(express())
+const serverConfigs: IServerConfiguration = {
+    port: env.SERVER_PORT || 3000
+}
+
+const app: ExpressServer = new ExpressServer(express(), serverConfigs)
 
 app.listen()
